f253b3341b3818ae98e4ce59808adb2ff8fc9486
dyrhave <126078101+dyrhave@users.noreply.github.com>
2023-08-22 10:05:25 +0200

    resize canvas function

--- main.js
+++ main.js
@@ -3,29 +3,56 @@ const rows = 16;
 const columns = 16;
 let isDrawing = false;
 
-for (let i = 0; i < rows * columns; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    container.appendChild(gridSquare);
-    
-    gridSquare.addEventListener('mousedown', startDraw);
+gridSizeSlider.addEventListener('input', updateGridSize)
+
+function updateGridSize() {
+    const newSize = gridSizeSlider.value;
+    clearGrid();
+    generateGrid(newSize)
+    gridSizeText.textContent = `Grid Size: ${newSize}`;
+}
+
+
+function clearGrid() {
+    container.innerHTML = '';
+}
+
+function generateGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        container.appendChild(gridSquare);
+    }
+
+    container.addEventListener('mousedown', startDraw);
+    container.addEventListener('mouseup', stopDraw);
+    container.addEventListener('mousemove', (e) => draw(e, 'black'));
 }
 
 container.addEventListener('mouseup', stopDraw);
-container.addEventListener('mousemove', draw);
+document.addEventListener('mousemove', (e) => {
+    if (isDrawing) {
+        draw(e, 'black');
+    }
+});
 
 function startDraw(e) {
     isDrawing = true;
-    draw(e);
+    draw(e, 'black');
 }
 
-function draw(e) {
+function draw(e, color) {
     if (!isDrawing) return;
-    e.target.style.backgroundColor = 'black';
+    e.target.style.backgroundColor = color;
 }
 
 function stopDraw() {
     isDrawing = false;
 }
 
-container.addEventListener('mousedown', (e) => e.preventDefault());
+document.addEventListener('DOMContentLoaded', () => {
+    generateGrid(16);
+})
