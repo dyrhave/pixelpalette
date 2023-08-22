3ad3301b4e4abd959de5ce94e4b1d61f64191db4
dyrhave <126078101+dyrhave@users.noreply.github.com>
2023-08-22 08:31:43 +0200

    change drawing style from hover to mousedown

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const container = document.getElementById('container');
-const rows = 16
-const columns = 16
+const rows = 16;
+const columns = 16;
+let isDrawing = false;
 
 for (let i = 0; i < rows * columns; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
     container.appendChild(gridSquare);
-    gridSquare.addEventListener('mouseenter', hoverGrid)
+    
+    gridSquare.addEventListener('mousedown', startDraw);
 }
 
-function hoverGrid(e) {
+container.addEventListener('mouseup', stopDraw);
+container.addEventListener('mousemove', draw);
+
+function startDraw(e) {
+    isDrawing = true;
+    draw(e);
+}
+
+function draw(e) {
+    if (!isDrawing) return;
     e.target.style.backgroundColor = 'black';
 }
 
+function stopDraw() {
+    isDrawing = false;
+}
+
+container.addEventListener('mousedown', (e) => e.preventDefault());
